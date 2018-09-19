782d24e19939fe6f27bca3315694cfa93104cc50
Shikamaakuma <shikamaakuma@users.noreply.github.com>
2018-09-19 15:37:30 +0200

    first draft on the connection function

--- Project Connect/connection.c
+++ Project Connect/connection.c
@@ -13,12 +13,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <curl.h>
+ 
+int postToWeb(char[] webpage, char[] toPost){
+    CURL *curl;
+    CURLcode res; 
+    
+    curl_global_init(CURL_GLOBAL_ALL);
+    
+    curl = curl_easy_init();
+    
+    if(curl) {
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+        curl_easy_setopt(curl, CURLOPT_URL, );
+        
+        curl_easy_setopt(curl, CURLOPT_URL, webpage);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, toPost);
+ 
+ 
+        res = curl_easy_perform(curl);
 
-    return (EXIT_SUCCESS);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+ 
+
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return 0;
 }
-
