b8c6fd3433f8095528f6b4c38a0ac758abd4fa2c
Shikamaakuma <Shikamaakuma@users.noreply.github.com>
2018-09-19 22:38:50 +0200

    added comments on connection.c

--- Project Connect/connection.c
+++ Project Connect/connection.c
@@ -19,26 +19,27 @@ int postToWeb(char[] webpage, char[] toPost){
     CURL *curl;
     CURLcode res; 
     
+	//initializes most things like socket and protocols
     curl_global_init(CURL_GLOBAL_ALL);
-    
+    //starts a libcurl session with curl as handle
     curl = curl_easy_init();
     
+	//if it could create a session -> needs exception catcher
     if(curl) {
-
-        curl_easy_setopt(curl, CURLOPT_URL, );
         
+		//sets the webpage and what to post for curl 
         curl_easy_setopt(curl, CURLOPT_URL, webpage);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, toPost);
  
- 
+		//posts toPost on website and returns a value depending on if it was successful or not
         res = curl_easy_perform(curl);
 
-
+		//in case the toPost was not successful
         if(res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
  
-
+		//kills curl and all associated memories
         curl_easy_cleanup(curl);
     }
     curl_global_cleanup();
