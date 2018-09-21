e9319082de1f90137abd1fe50f90d13c51a39ff3
Shikamaakuma <shikamaakuma@users.noreply.github.com>
2018-09-21 12:01:56 +0200

    fixed file_writer.php

--- UI/file_writer.php
+++ UI/file_writer.php
@@ -2,5 +2,5 @@
     $saveThis = $_POST[Winterthur_temp];
     $winterthur_Temp = fopen("winterthur_temp.txt", "w") or die("Unable to open file!");
     fwrite($winterthur_Temp, $saveThis);
-    fclose($myfile);
+    fclose($winterthur_Temp);
 ?> 
