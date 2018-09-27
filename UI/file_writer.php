c1b56393dfc026111326369f15387224065a050f
Shikamaakuma <shikamaakuma@users.noreply.github.com>
2018-09-27 10:12:09 +0200

    fixed file_writer.php once again

--- UI/file_writer.php
+++ UI/file_writer.php
@@ -1,13 +1,7 @@
-
-<?php
-    include 'DB.php';
-    $next = false;
-
-    
+ <?php
     if(isset($_POST[Winterthur_temp])){
         $w_temp = $_POST[Winterthur_temp];
         $winterthur_Temp = fopen("winterthur_temp.txt", "w") or die("Unable to open file!");
-        fwrite($winterthur_Temp, $w_temp);
         fwrite($winterthur_Temp, $w_temp);
         fclose($winterthur_Temp);
     }
@@ -16,14 +10,12 @@
         $w_press = $_POST[Winterthur_press];
         $winterthur_Press = fopen("winterthur_press.txt", "w") or die("Unable to open file!");
         fwrite($winterthur_Press, $w_press);
-        fwrite($winterthur_Press, $w_press);
         fclose($winterthur_Press);
     } 
     
     if(isset($_POST[Winterthur_airqual])){
         $w_air = $_POST[Winterthur_airqual];
         $winterthur_Air = fopen("winterthur_air.txt", "w") or die("Unable to open file!");
-        fwrite($winterthur_Air, $w_air);
         fwrite($winterthur_Air, $w_air);
         fclose($winterthur_Air);
     }
@@ -32,15 +24,6 @@
         $w_h2O = $_POST[Winterthur_h2o];
         $winterthur_H2o = fopen("winterthur_h2o.txt", "w") or die("Unable to open file!");
         fwrite($winterthur_H2o, $w_h2O);
-        fwrite($winterthur_H2o, $w_h2O);
         fclose($winterthur_H2o);
-        $next = true;
     }
-    
-    if($next = true){
-        $query = "INSERT INTO Winterthur(Temp,Press,Air,H2o)VALUES($w_temp,$w_press,$w_air,$w_h2o)";
-        mysqli_query($mysqli, $$query);
-        $next = false;
-    }
-    mysqli_close($mysqli);
 ?> 
