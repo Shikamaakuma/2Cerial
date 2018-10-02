d6691b56dba3c801773c012a9cd2bf6f023bdbe7
Ra1nerZufall <Ra1nerZufall@users.noreply.github.com>
2018-10-02 11:17:17 +0200

    changed both wetter_vergleich files
    should now work. not pretty but it's a start.

--- UI/WetterVergleich/wetter_vergleich.php
+++ UI/WetterVergleich/wetter_vergleich.php
@@ -1,20 +1,20 @@
 <?php/*
 	//reads temperature of Winterthur
     $winterthur_Temp = fopen("winterthur_temp.txt", "r") or die("Unable to open file!");
-    $temp = fgets($winterthur_Temp, 6);
+    $tempW = fgets($winterthur_Temp, 6);
 	fgets($winterthur_Temp);
-    $date = fgets($winterthur_Temp);
-    $time = fgets($winterthur_Temp);
+    $dateW = fgets($winterthur_Temp);
+    $timeW = fgets($winterthur_Temp);
     fclose($winterthur_Temp);
 	
 	// reads airpressure of Winterthur
     $winterthur_Press = fopen("winterthur_press.txt", "r") or die("Unable to open file!");
-    $press = fgets($winterthur_Press);
+    $pressW = fgets($winterthur_Press);
     fclose($winterthur_Press);
 	
 	// reads waterconcentration in air of Winterthur
     $winterthur_Temp = fopen("winterthur_h2o.txt", "r") or die("Unable to open file!");
-    $temp = fgets($winterthur_h2o);
+    $h2oW = fgets($winterthur_h2o);
     fclose($winterthur_h2o);
 	*/
 ?> 
@@ -29,70 +29,198 @@
 	<body>
 	<div id = "container">
 		<div id = "Winterthur">
-			<div class="leftbox">
-				<p class="ort"> Winterthur </p>
-				<div class="datetime">
-					<p class="time">
+			<!--rightboxstart-->
+				<p class="detail ort"> 
+					Winterthur 
+				</p><!--ort-->
+				<p class="detail time">
 					<?php
-						$time = "19:00"; /*for test purpose*/
-						echo $time; 
+						$timeW = "19:00"; /*for test purpose*/
+						echo $timeW; 
 					?>
-					</p><!--time-->
-					<p class ="date">
+				</p><!--time-->
+				<p class ="detail date">
 					<?php
-						$date = "20.09.2018"; /*for test purpose*/
-						echo $date;
+						$dateW = "20.09.2018"; /*for test purpose*/
+						echo $dateW;
 					?>
-					</p><!--date-->
-				</div><!--date/time-->
+				</p><!--date-->
 				
-				<p class ="temp">
-				
-				<?php
-					$temp = 20.5; /*for test purposes*/
-					echo $temp."°C";
-				?>
+				<p class ="detail temp">
+					<?php
+						$tempW = 20.5; /*for test purposes*/
+						echo $tempW."°C";
+					?>
 				</p><!--temp-->	
 				<?php?>
 				<!--
-				if ($temp > 30){
+				if ($tempW > 30){
 					echo"<style='background-color: '>";
 				}	
-				else if($temp <= 20 && $temp > 20){
+				else if($tempW <= 20 && $temp > 20){
 					echo "<style='background-color: '>";
 				}
-				else if($temp <= 20 && $temp >10){
+				else if($tempW <= 20 && $temp >10){
 					echo "<style='background-color: '>";
 				}
-				else if($temp <= 10 && $temp >0){
+				else if($tempW <= 10 && $temp >0){
 					echo "<style='background-color: '>";
 				}
-				else if($temp <= 0){
+				else if($tempW <= 0){
 					echo "<style='background-color: '>";
 				}*/
 				?>-->
-			</div><!--leftbox-->
-			<div class="detail">
-				<p class="pressText">
+			<!--rightboxend-->
+			<!--leftboxstart-->
+				<p class="detail pressText">
 					Luftdruck
 				</p><!--pressText-->
-				<p class="press">
+				<p class="detail press">
 					<?php
-						$press = 990;
-						echo $press;
+						$pressW = 990;
+						echo $pressW;
 					?>
 				</p><!--press-->
-				<p class="h2oText">
+				<p class="detail h2oText">
 					Luftfeuchtigkeit
 				</p><!--h2oText-->
-				<p class="h2o">
+				<p class="detail h2o">
 					<?php
-						$h2o = 25.7;
-						echo $h2o;
+						$h2oW = 25.7;
+						echo $h2oW;
 					?>
 				</p><!--h2o-->
-			</div><!--detail-->
+			<!--leftboxend-->
 		</div><!--Winterthur-->	
+		<div id = "Romanshorn">
+			<!--rightboxstart-->
+				<p class="detail ort"> 
+					Romanshorn 
+				</p><!--ort-->
+				<p class="detail time">
+					<?php
+						$timeR = "19:00"; /*for test purpose*/
+						echo $timeR; 
+					?>
+				</p><!--time-->
+				<p class ="detail date">
+					<?php
+						$dateR = "20.09.2018"; /*for test purpose*/
+						echo $dateR;
+					?>
+				</p><!--date-->
+				
+				<p class ="detail temp">
+					<?php
+						$tempR = 20.5; /*for test purposes*/
+						echo $tempR."°C";
+					?>
+				</p><!--temp-->	
+				<?php?>
+				<!--
+				if ($tempR > 30){
+					echo"<style='background-color: '>";
+				}	
+				else if($tempR <= 20 && $temp > 20){
+					echo "<style='background-color: '>";
+				}
+				else if($tempR <= 20 && $temp >10){
+					echo "<style='background-color: '>";
+				}
+				else if($tempR <= 10 && $temp >0){
+					echo "<style='background-color: '>";
+				}
+				else if($tempR <= 0){
+					echo "<style='background-color: '>";
+				}*/
+				?>-->
+			<!--rightboxend-->
+			<!--leftboxstart-->
+				<p class="detail pressText">
+					Luftdruck
+				</p><!--pressText-->
+				<p class="detail press">
+					<?php
+						$pressR = 990;
+						echo $pressR;
+					?>
+				</p><!--press-->
+				<p class="detail h2oText">
+					Luftfeuchtigkeit
+				</p><!--h2oText-->
+				<p class="detail h2o">
+					<?php
+						$h2oR = 25.7;
+						echo $h2oR;
+					?>
+				</p><!--h2o-->
+			<!--leftboxend-->
+		</div><!--Romanshorn-->	
+		<div id = "Neuhausen">
+			<!--rightboxstart-->
+				<p class="detail ort"> 
+					Neuhausen am Rheinfall
+				</p><!--ort-->
+				<p class="detail time">
+					<?php
+						$timeN = "19:00"; /*for test purpose*/
+						echo $timeN; 
+					?>
+				</p><!--time-->
+				<p class ="detail date">
+					<?php
+						$dateN = "20.09.2018"; /*for test purpose*/
+						echo $dateN;
+					?>
+				</p><!--date-->
+				
+				<p class ="detail temp">
+					<?php
+						$tempN = 20.5; /*for test purposes*/
+						echo $tempN."°C";
+					?>
+				</p><!--temp-->	
+				<?php?>
+				<!--
+				if ($tempN > 30){
+					echo"<style='background-color: '>";
+				}	
+				else if($tempN <= 20 && $temp > 20){
+					echo "<style='background-color: '>";
+				}
+				else if($tempN <= 20 && $temp >10){
+					echo "<style='background-color: '>";
+				}
+				else if($tempN <= 10 && $temp >0){
+					echo "<style='background-color: '>";
+				}
+				else if($tempN <= 0){
+					echo "<style='background-color: '>";
+				}*/
+				?>-->
+			<!--rightboxend-->
+			<!--leftboxstart-->
+				<p class="detail pressText">
+					Luftdruck
+				</p><!--pressText-->
+				<p class="detail press">
+					<?php
+						$pressN = 990;
+						echo $pressN;
+					?>
+				</p><!--press-->
+				<p class="detail h2oText">
+					Luftfeuchtigkeit
+				</p><!--h2oText-->
+				<p class="detail h2o">
+					<?php
+						$h2oN = 25.7;
+						echo $h2oN;
+					?>
+				</p><!--h2o-->
+			<!--leftboxend-->
+		</div><!--Neuhausen-->
+		
 	</div><!--container-->
 	</body>
 </html>
