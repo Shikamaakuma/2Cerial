8df1e3ff25668e05bebec88e8e3ba547d3084b53
Hallal <Hallal@users.noreply.github.com>
2018-10-16 13:30:53 +0200

    Fehlermeldung für Yahoo Prognose

--- UI/forecast_Winterthur.php
+++ UI/forecast_Winterthur.php
@@ -11,13 +11,24 @@
 		<div id="main" class="navbutton"><a href="../main_page.php">Haus</a></div>
 		<div id="comparison" class="navbutton"><a href="../weather_comparison.php">Standorte</a></div>
 </div>
-<div id="title">
-	<h1 class="info"> Prognosen für Winterthur </h1>
-</div>
+<?php
+if(isset($phpObj->query->results->channel->item->forecast)){
+	echo
+	"<div id='title'>
+		<h1 class='info'> Prognosen für Winterthur </h1>
+	</div>";
+}
+else{
+	echo
+	"<div class='title'>
+		<h1 class='info'>Prognose für Winterthur nicht verfügbar</h1>
+	</div>";
+}
+?>
 <div class="container">
 <?php
     $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
-    $yql_query = "select * from weather.forecast where woeid=784723 and u='c'";
+    $yql_query = 'select * from weather.forecast where woeid=784723 and u=\'c\' and datetime between date_sub(curdate(), interval 7 days) nd now()';
     $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
     // Make call with cURL
     $session = curl_init($yql_query_url);
@@ -158,77 +169,101 @@
 		}	
 	}
 	
-	//Kreieren der divs für zehn Tage ab heute
+	//Kreieren der divs für 8 Tage ab heute
 	$daily_data=1;
-	
-	for ($counter = 0; $counter < 8; $counter++){
-		$db_path = $phpObj->query->results->channel->item->forecast[$counter];
-		if ($db_path->high > 30){
-			$W = "T30";
-		}	
-		else if($db_path->high  <= 30 && $db_path->high > 20){
-			$W = "T20";
-		}
-		else if($db_path->high <= 20 && $db_path->high >10){
-			$W = "T10";
-		}
-		else if($db_path->high <= 10 && $db_path->high >0){
-			$W = "T0";
-		}
-		else if($db_path->high <= 0){
-			$W = "T-10";
-		}
-		echo "<div class='".$W." days'>";
-		
-		switch ($daily_data){
-			case 1:
-					echo "<div class='today'>";
-					echo "<h2>Heute</h2>";
-					break;
-			case 2:
-					echo "<div class='tomorrow'>";
-					echo "<h2>Morgen</h2>";
-					break;
-			case 3:
-					echo "<div class='daft1'>";
-					echo "<h2>Übermorgen</h2>";	
-					break;
-			case 4:
-					echo "<div class='daft2'>";
-					echo "<h2>In 3 Tagen</h2>";	
-					break;
-			case 5:
-					echo "<div class='daft3'>";
-					echo "<h2>In 4 Tagen</h2>";
-					break;
-			case 6:
-					echo "<div class='daft4'>";
-					echo "<h2>In 5 Tagen</h2>";	
-					break;
-			case 7:
-					echo "<div class='daft5'>";
-					echo "<h2>In 6 Tagen</h2>";	
-					break;
-			case 8:
-					echo "<div class='daft6'>";
-					echo "<h2>In 7 Tagen</h2>";	
-					break;
-		}
-		
-		$timestamp_foo=strtotime($db_path->date);
-		setlocale(LC_TIME, 'German');
-		echo "<p class='date'>".strftime('%A', $timestamp_foo)."</br>".strftime('%d. %B %Y', $timestamp_foo)."</br></p>";
-		
-		echo "<p class='high info'>Höchste Temperatur: ".$db_path->high . "°C</br></p>";
-		
-		echo "<p class='low info'>Niedrigste Temperatur: ".$db_path->low . "°C</br></p>";
+	if(isset($phpObj->query->results->channel->item->forecast)){
+		foreach ($phpObj->query->results->channel->item->forecast as $db_path){
+			if ($db_path->high > 30){
+				$W = "T30";
+			}	
+			else if($db_path->high  <= 30 && $db_path->high > 20){
+				$W = "T20";
+			}
+			else if($db_path->high <= 20 && $db_path->high >10){
+				$W = "T10";
+			}
+			else if($db_path->high <= 10 && $db_path->high >0){
+				$W = "T0";
+			}
+			else if($db_path->high <= 0){
+				$W = "T-10";
+			}
+			echo "<div class='".$W." days'>";
+			
+			switch ($daily_data){
+				case 1:
+						echo "<div class='today'>";
+						echo "<h2>Heute</h2>";
+						break;
+				case 2:
+						echo "<div class='tomorrow'>";
+						echo "<h2>Morgen</h2>";
+						break;
+				case 3:
+						echo "<div class='daft1'>";
+						echo "<h2>Übermorgen</h2>";	
+						break;
+				case 4:
+						echo "<div class='daft2'>";
+						echo "<h2>In 3 Tagen</h2>";	
+						break;
+				case 5:
+						echo "<div class='daft3'>";
+						echo "<h2>In 4 Tagen</h2>";
+						break;
+				case 6:
+						echo "<div class='daft4'>";
+						echo "<h2>In 5 Tagen</h2>";	
+						break;
+				case 7:
+						echo "<div class='daft5'>";
+						echo "<h2>In 6 Tagen</h2>";	
+						break;
+				case 8:
+						echo "<div class='daft6'>";
+						echo "<h2>In 7 Tagen</h2>";	
+						break;
+			}
+			$date=$db_path->date;
+			if(isset($date)){
+				$timestamp_foo=strtotime($date);
+				setlocale(LC_TIME, 'German');
+				echo "<p class='date'>".strftime('%A', $timestamp_foo)."</br>".strftime('%d. %B %Y', $timestamp_foo)."</br></p>";
+			}
+			else{
+				echo "Zeit nicht verfügbar";
+			}
+			
+			$high=$db_path->high;
+			if(isset($high)){
+				echo "<p class='high info'>Höchste Temperatur: ".$high . "°C</br></p>";
+			}
+			else{
+				echo "Höchste Temperatur nicht verfügbar";
+				echo "<p class='high info'>Höchste Temperatur nicht verfügbar</br></p>";
 
-		$text=$db_path->text;
-		echo "<p class='text info'>".transl_engl2de($text)."</br></p>";
-		
-		echo "</div>";
-		echo "</div>";
-		$daily_data++;
+			}
+			
+			$low=$db_path->low;
+			if(isset($low)){
+				echo "<p class='low info'>Niedrigste Temperatur: ".$low . "°C</br></p>";
+			}
+			else{
+				echo "Niedrigste Temperatur nicht verfügbar";
+			}
+			
+			$text=$db_path->text;
+			if(isset($text)){
+				echo "<p class='text info'>".transl_engl2de($text)."</br></p>";
+			}
+			else{
+				echo "Wetterbeschreibung nicht verfügbar";
+			}
+			
+			echo "</div>";
+			echo "</div>";
+			$daily_data++;
+		}
 	}
 ?>
 
