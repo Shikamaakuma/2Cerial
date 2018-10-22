7e762e1255d8075a065ac6cf230fbe9710e3ee0c
Ra1nerZufall <Ra1nerZufall@users.noreply.github.com>
2018-10-22 09:58:48 +0200

    Update tables/tables.php

--- tables/tables.php
+++ tables/tables.php
@@ -1,8 +1,31 @@
 <!DOCTYPE HTML>
+<?php
+/*messures temp for backgroundimage data*/
+	$saveThis = $_POST[Winterthur_temp];
+    $winterthur_Temp = fopen("winterthur_temp.txt", "r") or die("Unable to open file!");
+    $temp = fgets($winterthur_Temp, 5);
+    fclose($winterthur_Temp);
+/*change of the background image dependent on temperature*/
+	/*$temp = 40.5; /*for test purposes*/
+	if($temp > 40){
+		echo "<link rel='stylesheet' type='text/css' media='all and (orientation: landscape)' href='css/main_page_mustafar.css' />";
+	}
+	else if($temp <= 40 && $temp > 20){
+		echo "<link rel='stylesheet' type='text/css' media='all and (orientation: landscape)' href='css/main_page_warm.css' />";
+	}
+	else if($temp <= 20 && $temp > -20){
+		echo "<link rel='stylesheet' type='text/css' media='all and (orientation: landscape)' href='css/main_page_cold.css' />" ;
+	}
+	else if($temp < -20){
+		echo "<link rel='stylesheet' type='text/css' media='all and (orientation: landscape)' href='css/main_page_hoth.css' />";
+	}
+?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html" meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" media="all and (orientation: portrait)" href="css/navbar_mobile.css" />
         <link rel="stylesheet" type="text/css" media="all and (orientation: portrait)" href="css/archive_mobile.css" />
+		<link rel="stylesheet" type="text/css" media="all and (orientation: landscape)" href="css/navbar.css" />
 		<link rel="stylesheet" type="text/css" media="all and (orientation: landscape)" href="css/archive.css" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="js/jquery.canvasjs.min.js"></script>
@@ -11,9 +34,9 @@
 
     <body>
         <div id="nav">
-		<div id="weather" class="navbutton"><a href="weather_comparison.php">Standorte</a></div>
-		<div id="main" class="navbutton"><a href="main_page.php">Haus</a></div>
-		<div id="forecast" class="navbutton"><a href="forecast_Winterthur.php">Prognose</a></div>
+		<div id="left_button" class="navbutton"><a href="weather_comparison.php">Standorte</a></div>
+		<div id="middle_button" class="navbutton"><a href="index.php">Startseite</a></div>
+		<div id="right_button" class="navbutton"><a href="forecast_Winterthur.php">Prognose</a></div>
 	</div>
         <div id="graph" ></div>
     </body>
