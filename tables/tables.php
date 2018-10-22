6a8ed1bfeb86f9a1fb3e05126a522d1c9ce9071a
Shikamaakuma <shikamaakuma@users.noreply.github.com>
2018-10-22 09:51:46 +0200

    graph now working (upgrades may be comming)

--- tables/tables.php
+++ tables/tables.php
@@ -1,15 +1,20 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html" meta charset="utf-8" Access-Control-Allow-Origin:* />
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <meta http-equiv="Content-Type" content="text/html" meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" media="all and (orientation: portrait)" href="css/archive_mobile.css" />
+		<link rel="stylesheet" type="text/css" media="all and (orientation: landscape)" href="css/archive.css" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="js/jquery.canvasjs.min.js"></script>
         <script type="text/javascript" src="js/table_evaluator.js"></script>
-        <!--includes jquerry-->
     </head>
 
     <body>
-        <a href="main_page.php"> zurück </a>
-        <div id="graph" style="height: 370px; width: 100%;"></div>
+        <div id="nav">
+		<div id="weather" class="navbutton"><a href="weather_comparison.php">Standorte</a></div>
+		<div id="main" class="navbutton"><a href="main_page.php">Haus</a></div>
+		<div id="forecast" class="navbutton"><a href="forecast_Winterthur.php">Prognose</a></div>
+	</div>
+        <div id="graph" ></div>
     </body>
 </html>
